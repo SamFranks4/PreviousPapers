a897d56eb7a5be365f24dbbf6e7bf492be955851
SamFranks4 <samfranks2007@gmail.com>
2025-11-03 16:40:15 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Highlight the active sidebar link based on current page
+const sidebarLinks = document.querySelectorAll('.sidebar-links a');
+
+// Get the current page filename (e.g., "index.html", "previous-papers.html")
+const currentPage = window.location.pathname.split("/").pop();
+
+sidebarLinks.forEach(link => {
+    const linkPage = link.getAttribute('href');
+    if (linkPage === currentPage) {
+        link.classList.add('active');
+    } else {
+        link.classList.remove('active');
+    }
+});
